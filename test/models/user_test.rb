cb3445c89cdbade58e5c52cd124f6f4d9dfb4331
thunderenlight <thunderenlight@gmail.com>
2017-03-01 17:08:18 -0600

    adding gravatar and debugging

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
 	def setup
-		@user = User.new(name: "Exampler Stooge", email: "user@exampler.com")
+		@user = User.new(name: "Exampler Stooge", email: "user@exampler.com",
+						 password: "foobar", password_confirmation: "foobar")
 	end
 
 	test "should be valid" do 
@@ -11,7 +12,7 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "name should be present" do
-	    @user.name = ""
+	    @user.name = "	"
 	    assert_not @user.valid?
   	end
   	test "email should be present" do 
