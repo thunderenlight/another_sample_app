f813d484dd16079320b08f0cb0f698fb47f25583
thunderenlight <thunderenlight@gmail.com>
2017-03-02 16:04:22 -0600

    Finishing CRUD for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302024328) do
+ActiveRecord::Schema.define(version: 20170302214132) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -24,9 +24,11 @@ ActiveRecord::Schema.define(version: 20170302024328) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
